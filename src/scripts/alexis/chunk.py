1f8a558a9357603238c35d4d82362904128bec1e
AlexisEidelman <alexis.eidelman.pro@gmail.com>
2013-06-19 18:56:15 +0100

    debut

---
+++ src/scripts/alexis/chunk.py
@@ -0,0 +1,66 @@
+# -*- coding:utf-8 -*-
+#
+# This file is part of OpenFisca.
+# OpenFisca is a socio-fiscal microsimulation software
+# Copyright © 2013 Alexis Eidelman, Clément Schaff, Mahdi Ben Jelloul
+# Licensed under the terms of the GVPLv3 or later license
+# (see openfisca/__init__.py for details)
+
+# Script to compute the aggregates for all the referenced years
+
+from src.lib.simulation import SurveySimulation 
+from src.plugins.survey.aggregates import Aggregates
+from src.plugins.survey.inequality import Inequality
+from pandas import ExcelWriter, ExcelFile, HDFStore
+import os
+
+import pandas.rpy.common as com
+import pdb
+
+country = 'france'
+
+from src.countries.france.data.sources.config import destination_dir
+
+
+fname_all = "aggregates_inflated_loyers.xlsx"
+fname_all = os.path.join(destination_dir, fname_all)              
+num_output = None
+
+
+def test_chunk():
+    print "debut"
+    writer = None
+    years = range(2011,2012)
+    filename = destination_dir+'output3.h5'
+    store = HDFStore(filename)
+    for year in years:        
+        yr = str(year)
+#        fname = "Agg_%s.%s" %(str(yr), "xls")
+        simu = SurveySimulation()
+        simu.set_config(year = yr, country = country)
+        simu.set_param()
+        import time
+        
+        
+        deb3 = time.clock()           
+        sous_ech = None
+        simu.set_config(survey_filename='C:\\Til\\output\\to_run_leg.h5', num_table=3, chunk=3, print_missing=False)
+        simu.compute()
+ 
+#         agg3 = Aggregates()
+#         for ent in ['ind','men','foy','fam']:
+#             tab = simu.outputs.table3[ent]
+#             renam={}
+#             renam['wprm_'+ent] = 'wprm'
+#             tab = tab.rename(columns=renam)
+#         agg3.set_simulation(simu)
+#         agg3.compute()       
+        
+        fin_chunk  = time.clock()
+
+    store.close()
+    
+if __name__ == '__main__':
+
+    test_chunk()
+
