3f830d81da6c99fb673ea95d59d14314b0b67097
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2014-02-08 19:21:03 +0100

    Modifies default date

--- openfisca_qt/gui/config.py
+++ openfisca_qt/gui/config.py
@@ -50,7 +50,7 @@ EDIT_FILETYPES = (
     (_("Cython/Pyrex files"), ('.pyx', '.pxd', '.pxi')),
     (_("C files"), ('.c', '.h')),
     (_("C++ files"), ('.cc', '.cpp', '.cxx', '.h', '.hh', '.hpp', '.hxx')),
-    (_("OpenCL files"), ('.cl', )),
+    (_("OpenCL files"), ('.cl',)),
     (_("Fortran files"), ('.f', '.for', '.f77', '.f90', '.f95', '.f2k')),
     (_("Patch and diff files"), ('.patch', '.diff', '.rej')),
     (_("Batch files"), ('.bat', '.cmd')),
@@ -77,19 +77,19 @@ def _get_extensions(filetypes):
     return ftype_list
 
 EDIT_FILTERS = _get_filters(EDIT_FILETYPES)
-EDIT_EXT = _get_extensions(EDIT_FILETYPES)+['']
+EDIT_EXT = _get_extensions(EDIT_FILETYPES) + ['']
 
 # Extensions supported by Spyder's Variable explorer
-#IMPORT_EXT = iofuncs.iofunctions.load_extensions.values()
+# IMPORT_EXT = iofuncs.iofunctions.load_extensions.values()
 
 # Extensions that should be visible in Spyder's file/project explorers
 SHOW_EXT = ['.png', '.ico', '.svg']
 
 # Extensions supported by Spyder (Editor or Variable explorer)
-VALID_EXT = EDIT_EXT  #+IMPORT_EXT
+VALID_EXT = EDIT_EXT  # +IMPORT_EXT
 
 # Find in files include/exclude patterns
-INCLUDE_PATTERNS = [r'|'.join(['\\'+_ext+r'$' for _ext in EDIT_EXT if _ext])+\
+INCLUDE_PATTERNS = [r'|'.join(['\\' + _ext + r'$' for _ext in EDIT_EXT if _ext]) + \
                     r'|README|INSTALL',
                     r'\.pyw?$|\.ipy$|\.txt$|\.rst$',
                     '.']
@@ -97,7 +97,7 @@ EXCLUDE_PATTERNS = [r'\.pyc$|\.pyo$|\.orig$|\.hg|\.svn|build',
                     r'\.pyc$|\.pyo$|\.orig$|\.hg|\.svn']
 
 # Name filters for file/project explorers (excluding files without extension)
-NAME_FILTERS = ['*' + _ext for _ext in VALID_EXT + SHOW_EXT if _ext]+\
+NAME_FILTERS = ['*' + _ext for _ext in VALID_EXT + SHOW_EXT if _ext] + \
                ['README', 'INSTALL', 'LICENSE', 'CHANGELOG']
 
 DEFAULTS = [
@@ -126,7 +126,7 @@ DEFAULTS = [
               }),
             ('composition',
              {
-              'year': '2006',
+              'year': '2013',
               'nmen': 101,
               'x_axis':  'sal',
               'minrev': 0,
@@ -161,7 +161,7 @@ DEFAULTS = [
              {
               'enable': True,
               'country': 'france',
-              'datesim': '2009-01-01',
+              'datesim': '2013-01-01',
               'reformes_dir': None,
 #              'file/param': 'param'
               }),
@@ -199,8 +199,8 @@ DEFAULTS = [
 
 DEV = not __file__.startswith(sys.prefix)
 DEV = False
-CONF = UserConfig('openfisca', defaults=DEFAULTS, load=(not DEV), version='2.4.0',
-                  subfolder=SUBFOLDER, backup=True, raw_mode=True)
+CONF = UserConfig('openfisca', defaults = DEFAULTS, load = (not DEV), version = '2.4.0',
+                  subfolder = SUBFOLDER, backup = True, raw_mode = True)
 # Removing old .openfisca.ini location:
 old_location = osp.join(get_home_dir(), '.openfisca.ini')
 if osp.isfile(old_location):
@@ -217,7 +217,7 @@ def add_image_path(path):
         for directory in dirs:
             IMG_PATH.append(osp.join(path, directory))
 
-add_image_path(get_module_data_path('openfisca_qt.gui', relpath='images'))
+add_image_path(get_module_data_path('openfisca_qt.gui', relpath = 'images'))
 
 
 # TODO: from spyderlib.otherplugins import PLUGIN_PATH
@@ -226,7 +226,7 @@ if PLUGIN_PATH is not None:
     add_image_path(osp.join(PLUGIN_PATH, 'images'))
 
 
-def get_image_path(name, default="not_found.png"):
+def get_image_path(name, default = "not_found.png"):
     """Return image absolute path"""
     for img_path in IMG_PATH:
         full_path = osp.join(img_path, name)
@@ -235,17 +235,17 @@ def get_image_path(name, default="not_found.png"):
     if default is not None:
         return osp.abspath(osp.join(img_path, default))
 
-def get_icon( name, default=None ):
+def get_icon(name, default = None):
     """Return image inside a QIcon object"""
     if default is None:
         return QIcon(get_image_path(name))
     elif isinstance(default, QIcon):
-        icon_path = get_image_path(name, default=None)
+        icon_path = get_image_path(name, default = None)
         return default if icon_path is None else QIcon(icon_path)
     else:
         return QIcon(get_image_path(name, default))
 
-def get_image_label( name, default="not_found.png" ):
+def get_image_label(name, default = "not_found.png"):
     """Return image inside a QLabel object"""
     label = QLabel()
     label.setPixmap(QPixmap(get_image_path(name, default)))
@@ -253,7 +253,7 @@ def get_image_label( name, default="not_found.png" ):
 
 def font_is_installed(font):
     """Check if font is installed"""
-    return [fam for fam in QFontDatabase().families() if unicode(fam)==font]
+    return [fam for fam in QFontDatabase().families() if unicode(fam) == font]
 
 def get_family(families):
     """Return the first installed font family in family list"""
@@ -267,7 +267,7 @@ def get_family(families):
         return QFont().family()
 
 FONT_CACHE = {}
-def get_font(section, option=None):
+def get_font(section, option = None):
     """Get console font properties depending on OS and user options"""
     font = FONT_CACHE.get((section, option))
     if font is None:
@@ -275,36 +275,36 @@ def get_font(section, option=None):
             option = 'font'
         else:
             option += '/font'
-        families = CONF.get(section, option+"/family", None)
+        families = CONF.get(section, option + "/family", None)
         if families is None:
             return QFont()
         family = get_family(families)
         weight = QFont.Normal
-        italic = CONF.get(section, option+'/italic', False)
-        if CONF.get(section, option+'/bold', False):
+        italic = CONF.get(section, option + '/italic', False)
+        if CONF.get(section, option + '/bold', False):
             weight = QFont.Bold
-        size = CONF.get(section, option+'/size', 9)
+        size = CONF.get(section, option + '/size', 9)
         font = QFont(family, size, weight)
         font.setItalic(italic)
         FONT_CACHE[(section, option)] = font
     return font
 
-def set_font(font, section, option=None):
+def set_font(font, section, option = None):
     """Set font"""
     if option is None:
         option = 'font'
     else:
         option += '/font'
-    CONF.set(section, option+'/family', unicode(font.family()))
-    CONF.set(section, option+'/size', float(font.pointSize()))
-    CONF.set(section, option+'/italic', int(font.italic()))
-    CONF.set(section, option+'/bold', int(font.bold()))
+    CONF.set(section, option + '/family', unicode(font.family()))
+    CONF.set(section, option + '/size', float(font.pointSize()))
+    CONF.set(section, option + '/italic', int(font.italic()))
+    CONF.set(section, option + '/bold', int(font.bold()))
     FONT_CACHE[(section, option)] = font
 
 
-def get_shortcut(context, name, default=NoDefault):
+def get_shortcut(context, name, default = NoDefault):
     """Get keyboard shortcut (key sequence string)"""
-    return CONF.get('shortcuts', '%s/%s' % (context, name), default=default)
+    return CONF.get('shortcuts', '%s/%s' % (context, name), default = default)
 
 def set_shortcut(context, name, keystr):
     """Set keyboard shortcut (key sequence string)"""
@@ -330,24 +330,24 @@ def get_color_scheme(name):
         color_scheme[key] = CONF.get("color_schemes", "%s/%s" % (name, key))
     return color_scheme
 
-def set_color_scheme(name, color_scheme, replace=True):
+def set_color_scheme(name, color_scheme, replace = True):
     """Set syntax color scheme"""
     section = "color_schemes"
     names = CONF.get("color_schemes", "names", [])
     for key in COLOR_SCHEME_KEYS:
         option = "%s/%s" % (name, key)
-        value = CONF.get(section, option, default=None)
+        value = CONF.get(section, option, default = None)
         if value is None or replace or name not in names:
             CONF.set(section, option, color_scheme[key])
     names.append(unicode(name))
     CONF.set(section, "names", sorted(list(set(names))))
 
-def set_default_color_scheme(name, replace=True):
+def set_default_color_scheme(name, replace = True):
     """Reset color scheme to default values"""
     assert name in COLOR_SCHEME_NAMES
-    set_color_scheme(name, COLORS[name], replace=replace)
+    set_color_scheme(name, COLORS[name], replace = replace)
 
 for _name in COLOR_SCHEME_NAMES:
-    set_default_color_scheme(_name, replace=False)
+    set_default_color_scheme(_name, replace = False)
 CUSTOM_COLOR_SCHEME_NAME = "Custom"
-set_color_scheme(CUSTOM_COLOR_SCHEME_NAME, COLORS["Spyder"], replace=False)
+set_color_scheme(CUSTOM_COLOR_SCHEME_NAME, COLORS["Spyder"], replace = False)
