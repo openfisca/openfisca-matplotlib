54b34d4ad9ef50fb013f4b47318558c9ba4c65b8
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2015-03-21 13:49:40 +0100

    Fix dataframes

--- openfisca_matplotlib/dataframes.py
+++ openfisca_matplotlib/dataframes.py
@@ -23,36 +23,40 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-import pandas as pd
+import pandas
 
 from openfisca_core import decompositions
 from openfisca_matplotlib.utils import OutNode
 
 
 def data_frame_from_decomposition_json(simulation, decomposition_json = None, reference_simulation = None,
-        remove_null = False):
+        remove_null = False, label = True, name = False):
     # currency = simulation.tax_benefit_system.CURRENCY # TODO : put an option to add currency, for now useless
+
+    assert label or name, "At least label or name should be True"
     if decomposition_json is None:
         decomposition_json = decompositions.get_decomposition_json(simulation.tax_benefit_system)
     data = OutNode.init_from_decomposition_json(simulation, decomposition_json)
-    data_dict = dict()
-    index = []
+
+    index = [row.desc for row in data if row.desc not in ('root')]
+    data_frame = None
     for row in data:
         if row.desc not in ('root'):
-            index.append(row.desc)
-            data_dict[row.desc] = row.vals
+            if data_frame is None:
+                value_columns = ['value_' + str(i) for i in range(len(row.vals))] if len(row.vals) > 1 else ['value']
+                data_frame = pandas.DataFrame(index = index, columns = ['name'] + value_columns)
 
-    data_frame = pd.DataFrame(data_dict).T
-    data_frame = data_frame.reindex(index)
-    data_frame.index.name = "variable"
-    if len(data_frame.columns) == 1:
-        data_frame.columns = ["valeur"]
+            data_frame['name'][row.desc] = row.code
+            data_frame.loc[row.desc, value_columns] = row.vals
+
+    data_frame.index.name = "label"
     if remove_null:
         variables_to_remove = []
         for variable in data_frame.index:
-            if (data_frame.loc[variable] == 0).all():
+            if (data_frame.loc[variable, value_columns] == 0).all():
                 variables_to_remove.append(variable)
-
         data_frame.drop(variables_to_remove, inplace = True)
+
+    data_frame.reset_index(inplace = True)
 
     return data_frame
