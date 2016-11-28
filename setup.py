99eee10ce08be5fe8d8bda53b2e9845faca6e448
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2016-11-28 20:35:17 +0100

    Fix dependences

--- setup.py
+++ setup.py
@@ -2,28 +2,6 @@
 # -*- coding: utf-8 -*-
 
 
-# OpenFisca -- A versatile microsimulation software
-# By: OpenFisca Team <contact@openfisca.fr>
-#
-# Copyright (C) 2011, 2012, 2013, 2014 OpenFisca Team
-# https://github.com/openfisca
-#
-# This file is part of OpenFisca.
-#
-# OpenFisca is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-#
-# OpenFisca is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
 """Matplotlib graphs for OpenFisca -- a versatile microsimulation free software"""
 
 
@@ -44,7 +22,7 @@ doc_lines = __doc__.split('\n')
 
 setup(
     name = 'OpenFisca-Matplotlib',
-    version = '0.4.dev0',
+    version = '0.5b1',
 
     author = 'OpenFisca Team',
     author_email = 'contact@openfisca.fr',
@@ -57,7 +35,7 @@ setup(
 
     include_package_data = True,
     install_requires = [
-        'OpenFisca-Core >= 0.4dev',
+        'OpenFisca-France >= 6.0.1',
         ],
     packages = find_packages(),
     message_extractors = {
