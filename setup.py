5288fe41678fe6213a108580089bdccfdcd45ae7
Christophe Benz <christophe.benz@gmail.com>
2018-02-24 22:40:43 +0100

    Revert "Move PySide to extras_require"
    This reverts commit d34d11bbb20c9609d3f5c589521e9e07623c2068.

--- setup.py
+++ setup.py
@@ -38,12 +38,10 @@ setup(
         'matplotlib',
         'OpenFisca-France >= 6.0.1',
         'pandas',
+        'PySide',
         ],
     packages = find_packages(),
     message_extractors = {
-    extras_require={
-        'qt': ["PySide"],
-    }
         'openfisca_matplotlib': [
             ('**.py', 'python', None),
             ],
