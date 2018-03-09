f4bed58f58298b11ff3f67683854727fd5e0c450
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2018-03-09 14:33:28 +0100

    Fix MatplotlibWidget import

--- openfisca_matplotlib/widgets/matplotlibwidget.py
+++ openfisca_matplotlib/widgets/matplotlibwidget.py
@@ -20,9 +20,11 @@ try:
     from PyQt4.QtGui import QSizePolicy
     from PyQt4.QtCore import QSize
 except:
-    from PySide.QtGui import QSizePolicy
-    from PySide.QtCore import QSize
-
+    try:
+        from PySide.QtGui import QSizePolicy
+        from PySide.QtCore import QSize
+    except:
+        QSizePolicy, QSize = None, None
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as Canvas
 from matplotlib.figure import Figure
 
@@ -33,69 +35,73 @@ __version__ = "1.0.0"
 rcParams['font.size'] = 9
 
 
-class MatplotlibWidget(Canvas):
-    """
-    MatplotlibWidget inherits PyQt4.QtGui.QWidget
-    and matplotlib.backend_bases.FigureCanvasBase
+if QSize and QSizePolicy:
+    class MatplotlibWidget(Canvas):
+        """
+        MatplotlibWidget inherits PyQt4.QtGui.QWidget
+        and matplotlib.backend_bases.FigureCanvasBase
 
-    Options: option_name (default_value)
-    -------
-    parent (None): parent widget
-    title (''): figure title
-    xlabel (''): X-axis label
-    ylabel (''): Y-axis label
-    xlim (None): X-axis limits ([min, max])
-    ylim (None): Y-axis limits ([min, max])
-    xscale ('linear'): X-axis scale
-    yscale ('linear'): Y-axis scale
-    width (4): width in inches
-    height (3): height in inches
-    dpi (100): resolution in dpi
-    hold (False): if False, figure will be cleared each time plot is called
+        Options: option_name (default_value)
+        -------
+        parent (None): parent widget
+        title (''): figure title
+        xlabel (''): X-axis label
+        ylabel (''): Y-axis label
+        xlim (None): X-axis limits ([min, max])
+        ylim (None): Y-axis limits ([min, max])
+        xscale ('linear'): X-axis scale
+        yscale ('linear'): Y-axis scale
+        width (4): width in inches
+        height (3): height in inches
+        dpi (100): resolution in dpi
+        hold (False): if False, figure will be cleared each time plot is called
 
-    Widget attributes:
-    -----------------
-    figure: instance of matplotlib.figure.Figure
-    axes: figure axes
+        Widget attributes:
+        -----------------
+        figure: instance of matplotlib.figure.Figure
+        axes: figure axes
 
-    Example:
-    -------
-    self.widget = MatplotlibWidget(self, yscale='log', hold=True)
-    from numpy import linspace
-    x = linspace(-10, 10)
-    self.widget.axes.plot(x, x**2)
-    self.wdiget.axes.plot(x, x**3)
-    """
-    def __init__(self, parent=None, title='', xlabel='', ylabel='',
-                 xlim=None, ylim=None, xscale='linear', yscale='linear',
-                 width=4, height=3, dpi=100, hold=False):
-        self.figure = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = self.figure.add_subplot(111)
-        self.axes.set_title(title)
-        self.axes.set_xlabel(xlabel)
-        self.axes.set_ylabel(ylabel)
-        if xscale is not None:
-            self.axes.set_xscale(xscale)
-        if yscale is not None:
-            self.axes.set_yscale(yscale)
-        if xlim is not None:
-            self.axes.set_xlim(*xlim)
-        if ylim is not None:
-            self.axes.set_ylim(*ylim)
-        self.axes.hold(hold)
+        Example:
+        -------
+        self.widget = MatplotlibWidget(self, yscale='log', hold=True)
+        from numpy import linspace
+        x = linspace(-10, 10)
+        self.widget.axes.plot(x, x**2)
+        self.wdiget.axes.plot(x, x**3)
+        """
+        def __init__(self, parent=None, title='', xlabel='', ylabel='',
+                    xlim=None, ylim=None, xscale='linear', yscale='linear',
+                    width=4, height=3, dpi=100, hold=False):
+            self.figure = Figure(figsize=(width, height), dpi=dpi)
+            self.axes = self.figure.add_subplot(111)
+            self.axes.set_title(title)
+            self.axes.set_xlabel(xlabel)
+            self.axes.set_ylabel(ylabel)
+            if xscale is not None:
+                self.axes.set_xscale(xscale)
+            if yscale is not None:
+                self.axes.set_yscale(yscale)
+            if xlim is not None:
+                self.axes.set_xlim(*xlim)
+            if ylim is not None:
+                self.axes.set_ylim(*ylim)
+            self.axes.hold(hold)
 
-        Canvas.__init__(self, self.figure)
-        self.setParent(parent)
+            Canvas.__init__(self, self.figure)
+            self.setParent(parent)
 
-        Canvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
-        Canvas.updateGeometry(self)
+            Canvas.setSizePolicy(self, QSizePolicy.Expanding, QSizePolicy.Expanding)
+            Canvas.updateGeometry(self)
 
-    def sizeHint(self):
-        w, h = self.get_width_height()
-        return QSize(w, h)
+        def sizeHint(self):
+            w, h = self.get_width_height()
+            return QSize(w, h)
 
-    def minimumSizeHint(self):
-        return QSize(10, 10)
+        def minimumSizeHint(self):
+            return QSize(10, 10)
+
+else:
+    MatplotlibWidget = None
 
 
 if __name__ == '__main__':
